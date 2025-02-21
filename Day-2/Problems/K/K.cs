9d90c28d9306faa56c5a0621c0adbbf9ebb29fca
noouh <noouhfff@gmail.com>
2025-02-21 02:28:31 +0200

    [Feat]:Problem K

---
+++ Day-2/Problems/K/K.cs
@@ -0,0 +1,9 @@
+﻿string input = Console.ReadLine();
+string[] parts = input.Split(' ');
+long n = int.Parse(parts[0]);
+long n1 = int.Parse(parts[1]);
+long n2 = int.Parse(parts[2]);
+
+long max = Math.Max(Math.Max(n, n1), n2);
+long min = Math.Min(Math.Min(n, n1), n2);
+Console.Write($"{min} {max}");
