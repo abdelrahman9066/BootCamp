98b45ecab55a3eed04eae117977a285b0e03e75d
noouh <noouhfff@gmail.com>
2025-02-21 02:22:19 +0200

    [Feat]:Problem I

--- Day-2/Problems/I/I.cs
+++ Day-2/Problems/I/I.cs
@@ -3,4 +3,4 @@ string[] parts = input.Split(' ');
 long n = int.Parse(parts[0]);
 long n1 = int.Parse(parts[1]);
 
-Console.WriteLine(n%n1==0 || n1%n==0 ? "Multiples" : "No Multiples");
+Console.WriteLine(n>=n1 ? "Yes" : "No");
