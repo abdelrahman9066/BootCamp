c7beb69455bf77ab0a0e080bf9e1775a230a1e46
noouh <noouhfff@gmail.com>
2025-03-03 14:43:12 +0200

    [Feat]:Problem O

---
+++ Day-4/Problems/O.cs
@@ -0,0 +1,23 @@
+﻿string input = Console.ReadLine();
+        
+char op = input.First(c => "+-*/".Contains(c)); 
+string[] parts = input.Split(op); 
+int A = int.Parse(parts[0]);
+int B = int.Parse(parts[1]);
+
+if (op == '+')
+{
+    Console.WriteLine(A + B);
+}
+else if (op == '-')
+{
+    Console.WriteLine(A - B);
+}
+else if (op == '*')
+{
+    Console.WriteLine(A * B);
+}
+else
+{
+    Console.WriteLine(A / B);
+}
