315a46f4c1dccb335e8f491b2d680a02ca23562c
noouh <noouhfff@gmail.com>
2025-03-17 21:48:03 +0200

    [Add]:Problem U

---
+++ Day-8/Problems/U.cs
@@ -0,0 +1,19 @@
+﻿string input = Console.ReadLine();
+if (input.Contains("."))
+{
+    string[] parts = input.Split('.');
+    string integerPart = parts[0];
+    string decimalPart = parts[1];
+
+    if (decimalPart.TrimEnd('0') == "")
+    {
+        Console.WriteLine($"int {integerPart}");
+    }
+    else
+    {
+        Console.WriteLine($"float {integerPart} 0.{decimalPart}");
+
+    }
+}
+
+
