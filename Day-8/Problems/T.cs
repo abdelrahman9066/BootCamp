c41fa84ef9fe3dc2fa192a240de3b63a8a9fb381
noouh <noouhfff@gmail.com>
2025-03-17 21:08:25 +0200

    [Add]:Problem T

---
+++ Day-8/Problems/T.cs
@@ -0,0 +1,15 @@
+﻿string[] input = Console.ReadLine().Split();
+int x = int.Parse(input[0]);
+int y = int.Parse(input[1]);
+int z = int.Parse(input[2]);
+
+int[] arr = {x,y,z};
+Array.Sort(arr);
+foreach (int num in arr)
+{
+    Console.WriteLine(num);
+}
+Console.WriteLine();
+Console.WriteLine(x);
+Console.WriteLine(y);
+Console.WriteLine(z);
